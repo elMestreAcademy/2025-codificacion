985eaebdbf62bcdf46f0623000eff241779ef359
elMestre <carmesce@gmail.com>
2025-04-07 12:19:53 +0200

    Propuesta de solución (CLI)

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+def solucion_molona(cols: int) -> None:
+    MAX = 255
+    for i in range(0, MAX):
+        if not (i % cols):
+            print()
+
+        code = f"{i}".rjust(len(f"{MAX}"))
+        if i not in [7, 8, 9, 10, 13]:
+            caracter = chr(i)
+        else:
+            caracter = ""
+
+        print(code, ":", caracter, " ", end="")
+
+
 if __name__ == "__main__":
-    print(chr(98))
-    print("=======")
-    print(ord('c'))
+    solucion_molona(8)
